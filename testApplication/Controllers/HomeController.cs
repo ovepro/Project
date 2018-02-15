0d4bbc0d8b5dcb74bec69a0fdb6eb2fc42a16ab1
ovepro <mamun.al67@gmail.com>
2018-02-15 02:31:28 -0600

    Initial commit

---
+++ testApplication/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using testApplication.Models;
+
+namespace testApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Rootobject viewModel = new Rootobject();
+            return View();
+        }
+
+        string Baseurl = "http://services.groupkt.com/";
+        string requestUri = "state/get/USA/all/";
+
+        
+        public async Task<ActionResult> searchMethod ( string serach)
+        {
+            Rootobject searchInfo = new Rootobject();
+            Restresponse responseInfo = new Restresponse();
+            Result resultInfo = new Result();
+
+            using (var client = new HttpClient())
+            {
+                
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();                
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));                
+                HttpResponseMessage Res = await client.GetAsync(requestUri);                 
+                if (Res.IsSuccessStatusCode)
+                {                    
+                    var searchResponse = Res.Content.ReadAsStringAsync().Result;
+                     searchInfo = JsonConvert.DeserializeObject<Rootobject>(searchResponse);
+                   
+                }
+                return Json(searchInfo,JsonRequestBehavior.AllowGet);                   
+            }
+        }
+             
+    }
+}
---
+++ testApplication/Models/indexViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testApplication.Models
+{
+    [Serializable]
+    public class Rootobject
+    {
+        public Restresponse RestResponse { get; set; }
+    }
+
+    [Serializable]
+    public class Restresponse
+    {
+        public string[] messages { get; set; }
+        public Result[] result { get; set; }
+        
+    }
+
+    [Serializable]
+    public class Result
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("abbr")]
+        public string abbr { get; set; }
+        [JsonProperty("area")]
+        public string area { get; set; }
+        [JsonProperty("largest_city")]
+        public string largest_City { get; set; }
+        [JsonProperty("capital")]
+        public string capital { get; set; }
+    }
+
+}
